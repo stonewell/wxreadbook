4d8391ac912ac697f3b48e6d6b4ae48ddd6b9a21
jingnan.si <jingnan.si@000ab626-6751-0410-bef8-bfb07b3be5e9>
2008-07-10 02:36:33 +0000

    1)Move View related function from BufferedDoc to BufferedView

--- src/ReadBookBufferedDoc.h
+++ src/ReadBookBufferedDoc.h
@@ -7,6 +7,13 @@ typedef std::vector<wxFileOffset> wxArrayFileOffset;
 #include <map>
 typedef std::map<wxInt32, wxFileOffset> wxMapFileOffset;
 
+class IContentHelper
+{
+public:
+	virtual bool CouldBeShowInSingleLine(const wxString & strLine) const = 0;
+	virtual const wxInt32 GetCharsPerLine() const = 0;
+};
+
 class CReadBookBufferedDoc :
 	public CReadBookDoc
 {
@@ -20,24 +27,13 @@ public:
 	virtual const wxString & GetLine(wxInt32 nRow);
 	virtual wxInt32 GetBufferSize(void) const;
 
-	virtual wxInt32 GetBufferCurPos(void);
-	
-	virtual const wxInt32 GetCharsPerLine() const { return m_nCharsPerLine; };
-	virtual void SetCharsPerLine(wxInt32 charsPerLine);
-
 	virtual void ShiftStream(wxInt32 delta);
 
 	virtual wxInt32 OffsetToRow(wxInt32 nOffset);
 	virtual wxFileOffset RowToOffset(wxInt32 nRow);
 
-	virtual const wxInt32 GetCharSize() const { return m_nCharSize; }
-	virtual void SetCharSize(wxInt32 charSize) { m_nCharSize = charSize; }
-
-	virtual const wxInt32 GetMBCharSize() const { return m_nMBCharSize; }
-	virtual void SetMBCharSize(wxInt32 mbCharSize) { m_nMBCharSize = mbCharSize; }
-
-	virtual const wxInt32 GetLineSize() const { return m_nLineSize; }
-	virtual void SetLineSize(wxInt32 lineSize) { m_nLineSize = lineSize; }
+	const IContentHelper * GetContentHelper() const { return m_pContentHelper; }
+	void SetContentHelper(const IContentHelper * pContentHelper) { m_pContentHelper = pContentHelper; }
 protected:
 	virtual wxInt32 GetOffsetRow(wxFileOffset nOffset);
 	virtual bool LoadBuffer(const wxString & url, wxMBConv * conv, bool bGuess);
@@ -51,7 +47,6 @@ private:
 	wxInputStream * m_pInput;
 	wxMBConv * m_pConv;
 
-	wxInt32 m_nCharsPerLine;
 	wxFileOffset m_nFileLength;
 	
 	wxInt32 m_nLastReadRow;
@@ -62,9 +57,7 @@ private:
 
 	wxMapFileOffset m_RowOffsetMap;
 
-	wxInt32 m_nCharSize;
-	wxInt32 m_nMBCharSize;
-	wxInt32 m_nLineSize;
+	const IContentHelper * m_pContentHelper;
 
 	void CleanUp();
 	wxChar NextChar();
