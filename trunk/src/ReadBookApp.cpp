1f1f126f8f918ddb36046ffe592683a56d787dcd
jingnan.si <jingnan.si@000ab626-6751-0410-bef8-bfb07b3be5e9>
2010-09-16 09:24:30 +0000

    fix id range in wx2.9.1

--- trunk/src/ReadBookApp.cpp
+++ trunk/src/ReadBookApp.cpp
@@ -61,36 +61,38 @@ CReadBookMainFrm * g_pMainFrm = NULL;
 
 IMPLEMENT_APP(CReadBookApp)
 
-CReadBookApp::CReadBookApp(void)
-{
-    m_pDocManager = (wxDocManager *) NULL;
+CReadBookApp::CReadBookApp(void) {
+	m_pDocManager = (wxDocManager *) NULL;
 }
 
 extern void Initialize7ZipClassFactories();
 extern void Deinitialize7ZipClassFactories();
 
-bool CReadBookApp::OnInit(void)
-{
+bool CReadBookApp::OnInit(void) {
 	wxFileSystem::AddHandler(new wxArchiveFSHandler());
 	wxFileSystem::AddHandler(new wxFilterFSHandler);
 	Initialize7ZipClassFactories();
 
 	LoadPreference();
 
-	//// Create a document manager
-    m_pDocManager = new CReadBookDocManager;
 
-    //// Create a template relating drawing documents to their views
+	//// Create a document manager
+	m_pDocManager = new CReadBookDocManager;
+
+
+	//// Create a template relating drawing documents to their views
 	wxString archiveExts = GetPreference()->GetArchiveFileFilters();
 
-	if (archiveExts.Len() > 0) archiveExts += wxT(";");
+	if (archiveExts.Len() > 0)
+		archiveExts += wxT(";");
 
 	archiveExts += wxT("*.zip;*.tar;*.gz");
 
+
 #ifdef _WIN32
 #ifdef _WIN32_WCE
-//#define DOC_CLASS CReadBookSimpleDoc
-//#define VIEW_CLASS CReadBookSimpleView
+	//#define DOC_CLASS CReadBookSimpleDoc
+	//#define VIEW_CLASS CReadBookSimpleView
 #define DOC_CLASS CReadBookTPLDoc2
 #define VIEW_CLASS CReadBookTPLView2
 #else
@@ -101,107 +103,84 @@ bool CReadBookApp::OnInit(void)
 #define DOC_CLASS CReadBookTPLDoc2
 #define VIEW_CLASS CReadBookTPLView2
 #endif
-	(void) new wxDocTemplate(m_pDocManager,
-        wxT("ReadBook Files"),
-        wxT("*.txt;*.html;*.htm;") + archiveExts,
-        wxT(""),
-        wxT("rbk"),
-        wxT("ReadBook Doc"),
-        wxT("ReadBook View"),
-        CLASSINFO(DOC_CLASS), CLASSINFO(VIEW_CLASS));
+	(void) new wxDocTemplate(m_pDocManager, wxT("ReadBook Files"),
+			wxT("*.txt;*.html;*.htm;") + archiveExts, wxT(""), wxT("rbk"),
+			wxT("ReadBook Doc"), wxT("ReadBook View"), CLASSINFO(DOC_CLASS),
+			CLASSINFO(VIEW_CLASS));
 
-    (void) new wxDocTemplate(m_pDocManager,
-        wxT("ReadBook Text Files"),
-        wxT("*.txt;*.html;*.htm"),
-        wxT(""),
-        wxT("rbk"),
-        wxT("ReadBook Doc"),
-        wxT("ReadBook View"),
-        CLASSINFO(DOC_CLASS), CLASSINFO(VIEW_CLASS));
+	(void) new wxDocTemplate(m_pDocManager, wxT("ReadBook Text Files"),
+			wxT("*.txt;*.html;*.htm"), wxT(""), wxT("rbk"), wxT("ReadBook Doc"),
+			wxT("ReadBook View"), CLASSINFO(DOC_CLASS), CLASSINFO(VIEW_CLASS));
 
-    (void) new wxDocTemplate(m_pDocManager,
-        wxT("ReadBook Archive Files"),
-        archiveExts,
-        wxT(""),
-        wxT("rbk"),
-        wxT("ReadBook Doc"),
-        wxT("ReadBook View"),
-        CLASSINFO(DOC_CLASS), CLASSINFO(VIEW_CLASS));
+	(void) new wxDocTemplate(m_pDocManager, wxT("ReadBook Archive Files"),
+			archiveExts, wxT(""), wxT("rbk"), wxT("ReadBook Doc"),
+			wxT("ReadBook View"), CLASSINFO(DOC_CLASS), CLASSINFO(VIEW_CLASS));
 
-	(void) new wxDocTemplate(m_pDocManager,
-        wxT("All Files"),
-        wxT("*.*"),
-        wxT(""),
-        wxT(""),
-        wxT("ReadBook Doc"),
-        wxT("ReadBook View"),
-        CLASSINFO(DOC_CLASS), CLASSINFO(VIEW_CLASS));
+	(void) new wxDocTemplate(m_pDocManager, wxT("All Files"), wxT("*.*"),
+			wxT(""), wxT(""), wxT("ReadBook Doc"), wxT("ReadBook View"),
+			CLASSINFO(DOC_CLASS), CLASSINFO(VIEW_CLASS));
+
 
 #ifdef __WXMAC__
-    wxFileName::MacRegisterDefaultTypeAndCreator( wxT("rbk") , 'WXMB' , 'WXMA' ) ;
+	wxFileName::MacRegisterDefaultTypeAndCreator( wxT("rbk") , 'WXMB' , 'WXMA' );
 #endif
 
-    m_pDocManager->SetMaxDocsOpen(1);
+	m_pDocManager->SetMaxDocsOpen(1);
 
 	InitObjectCache();
 
-    //// Create the main g_pMainFrm window
-    g_pMainFrm = new CReadBookMainFrm(m_pDocManager,
-        (wxFrame *) NULL,
-        wxID_ANY,
-        wxT("ReadBook wxWidget"),
-        wxPoint(0, 0),
-        wxSize(500, 400),
-        wxDEFAULT_FRAME_STYLE);
 
-    //// Give it an icon (this is ignored in MDI mode: uses resources)
-    g_pMainFrm->SetIcon(wxICON(BOOK06));
+	//// Create the main g_pMainFrm window
+	g_pMainFrm = new CReadBookMainFrm(m_pDocManager, (wxFrame *) NULL, wxID_ANY,
+			wxT("ReadBook wxWidget"), wxPoint(0, 0), wxSize(500, 400),
+			wxDEFAULT_FRAME_STYLE);
 
-    g_pMainFrm->Init();
-    g_pMainFrm->Centre(wxBOTH);
-    g_pMainFrm->Show(true);
 
-    SetTopWindow(g_pMainFrm);
+	//// Give it an icon (this is ignored in MDI mode: uses resources)
+	g_pMainFrm->SetIcon(wxICON(BOOK06));
 
-    g_pMainFrm->Maximize(true);
+	g_pMainFrm->Init();
+	g_pMainFrm->Centre(wxBOTH);
+	g_pMainFrm->Show(true);
 
-    wxCommandEvent event(wxEVT_COMMAND_MENU_SELECTED, wxID_NEW);
+	SetTopWindow(g_pMainFrm);
 
-    g_pMainFrm->GetEventHandler()->ProcessEvent(event);
+	g_pMainFrm->Maximize(true);
 
-    return true;
+	wxCommandEvent event(wxEVT_COMMAND_MENU_SELECTED, wxID_NEW);
+
+	g_pMainFrm->GetEventHandler()->ProcessEvent(event);
+
+	return true;
 }
 
-int CReadBookApp::OnExit(void)
-{
+int CReadBookApp::OnExit(void) {
 	ReleaseObjectCache();
 
-    delete m_pDocManager;
+	delete m_pDocManager;
 	SavePreference();
 
 	Deinitialize7ZipClassFactories();
 
-    return 0;
+	return 0;
 }
 
-void CReadBookApp::LoadPreference()
-{
+void CReadBookApp::LoadPreference() {
 	const wxString & strFileName = GetConfigFileName();
 
-	if (wxFileName::FileExists(strFileName) && wxFileName::IsFileReadable(strFileName))
-	{
-		wxFileInputStream input( strFileName );
+	if (wxFileName::FileExists(strFileName) && wxFileName::IsFileReadable(
+			strFileName)) {
+		wxFileInputStream input(strFileName);
 
 		m_ReadBookPreference.Load(input);
 	}
 }
 
-void CReadBookApp::SavePreference()
-{
+void CReadBookApp::SavePreference() {
 	const wxString & strFileName = GetConfigFileName();
 
-	if (wxFileName::FileExists(strFileName) && !wxFileName::IsFileWritable(strFileName))
-	{
+	if (wxFileName::FileExists(strFileName) && !wxFileName::IsFileWritable(
+			strFileName)) {
 		return;
 	}
 
@@ -210,24 +189,21 @@ void CReadBookApp::SavePreference()
 	m_ReadBookPreference.Store(output);
 }
 
-const wxString CReadBookApp::GetConfigFileName()
-{
+const wxString CReadBookApp::GetConfigFileName() {
 	wxStandardPaths stdPath;
 
 	wxString dataDir = stdPath.GetUserDataDir();
 
-	if (!::wxDirExists(dataDir))
-	{
+	if (!::wxDirExists(dataDir)) {
 		wxFileName::Mkdir(dataDir, 0777, wxPATH_MKDIR_FULL);
 	}
 
-	wxFileName filename(dataDir,wxT("ReadBook.dat"));
+	wxFileName filename(dataDir, wxT("ReadBook.dat"));
 
 	return filename.GetFullPath();
 }
 
-void CReadBookApp::OpenRelatedFile(const wxString & fileName, wxInt16 nDelta)
-{
+void CReadBookApp::OpenRelatedFile(const wxString & fileName, wxInt16 nDelta) {
 	if (fileName.Length() == 0)
 		return;
 
@@ -241,19 +217,16 @@ void CReadBookApp::OpenRelatedFile(const wxString & fileName, wxInt16 nDelta)
 
 	wxString name = fs.FindFirst(wxT("*"));
 
-    wxString currentName = fileName;
+	wxString currentName = fileName;
 
-	while (name.Length() > 0)
-	{
+	while (name.Length() > 0) {
 		wxFileName fn1(name);
 
-		if (CanHandleFile(fn1))
-		{
+		if (CanHandleFile(fn1)) {
 			files.Add(name);
 
-			if (fn1.GetFullPath().CmpNoCase(fn.GetFullPath()) == 0)
-			{
-			    currentName = name;
+			if (fn1.GetFullPath().CmpNoCase(fn.GetFullPath()) == 0) {
+				currentName = name;
 			}
 		}
 
@@ -264,13 +237,11 @@ void CReadBookApp::OpenRelatedFile(const wxString & fileName, wxInt16 nDelta)
 
 	wxInt16 index = files.Index(currentName, true, false);
 
-	if (index >= 0)
-	{
+	if (index >= 0) {
 		index += nDelta;
 
-		if (index >= 0 && (size_t)index < files.Count())
-		{
-           m_pDocManager->CreateDocument(files.Item(index), wxDOC_SILENT);
+		if (index >= 0 && (size_t) index < files.Count()) {
+			m_pDocManager->CreateDocument(files.Item(index), wxDOC_SILENT);
 		}
 	}
 }
